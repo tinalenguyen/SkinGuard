2a91f05d50fc3334be106173b63f25f8eff27ddf
tinalenguyen <thetinalenguyen@gmail.com>
2023-10-22 03:57:45 -0400

    Added results page

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, render_template, redirect, session
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="8CUbAAkdfWfm2vpOmv2n")
-project = rf.workspace().project("melanoma-cancer")
-model = project.version(1).model
+
 
 # # infer on a local image
 # print(model.predict("melanomaexp.jpeg", confidence=40, overlap=30).json())
@@ -14,6 +12,7 @@ model = project.version(1).model
 
 app = Flask(__name__)
 app.secret_key = "skin"
+photos = UploadSet('photos', IMAGES)
 
 def logged_in():
     return "user" in session
@@ -33,14 +32,22 @@ def about():
 
 @app.route("/test", methods=['GET', 'POST'])
 def test():
-    return render_template("test.html")
-    
-@app.route("/submit")
-def submit():
-    inputVal = request.args.get("inputVal")
-    model.predict(inputVal, confidence=40, overlap=30).save("prediction.jpg")
-    
+    if (request.method == 'POST'):
+        inputVal = request.form.get("fileInput")
 
+        rf = Roboflow(api_key="8CUbAAkdfWfm2vpOmv2n")
+        project = rf.workspace().project("melanoma-cancer")
+        model = project.version(1).model
+
+        model.predict(inputVal, confidence=40, overlap=30).save("prediction.jpg")
+        return redirect("/results")
+    
+    else:
+        return render_template("test.html")
+    
+@app.route("/results", methods=['GET', 'POST'])
+def results():
+    return render_template("result.html")
     # rest of the code
 if __name__ == "__main__": 
     app.debug = True
