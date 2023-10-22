05c315b1217c62625c5ea0e194aff0c3eab3ffa2
tinalenguyen <thetinalenguyen@gmail.com>
2023-10-22 02:12:47 -0400

    Added to HTML Files and added RoboFlow

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, render_template, redirect, session
+from roboflow import Roboflow
 
-from flask import Flask, request, render_template, redirect, session
+rf = Roboflow(api_key="8CUbAAkdfWfm2vpOmv2n")
+project = rf.workspace().project("melanoma-cancer")
+model = project.version(1).model
+
+# # infer on a local image
+# print(model.predict("melanomaexp.jpeg", confidence=40, overlap=30).json())
+
+# # visualize your prediction
+# model.predict("melanomaexp.jpeg", confidence=40, overlap=30).save("prediction.jpg")
+
 
 app = Flask(__name__)
 app.secret_key = "skin"
@@ -21,7 +31,17 @@ def start():
 def about():
     return render_template("about.html")
 
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+    return render_template("test.html")
+    
+@app.route("/submit")
+def submit():
+    inputVal = request.args.get("inputVal")
+    model.predict(inputVal, confidence=40, overlap=30).save("prediction.jpg")
+    
 
+    # rest of the code
 if __name__ == "__main__": 
     app.debug = True
     app.run()
